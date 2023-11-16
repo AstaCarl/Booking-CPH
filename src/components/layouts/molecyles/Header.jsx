b0713cb68f4ff1ba289829d2565b0d3c22da19bc
Julie <jc.lauesen@gmail.com>
2023-11-16 13:58:17 +0100

    header

--- src/components/layouts/molecyles/Header.jsx
+++ src/components/layouts/molecyles/Header.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react';
 import { Container, Group, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './HeaderSimple.module.css';
+import {IconUserCircle} from '@tabler/icons-react';
 
+{/*Links */}
 const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
+  { link: '/about', label: 'Book et lokale' },
 ];
 
 export function Header() {
@@ -31,11 +30,24 @@ export function Header() {
 
   return (
     <header className={classes.header}>
-      <Container size="md" className={classes.inner}>
-        {/* <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group> */}
 
+      <Container size="ml" className={classes.inner}>
+        {/*Dette er vores logo */}
+        <div className={classes.logo}>
+          <img src="/cphbusiness_payoff_neg-1854815586.png" alt="Logo" />
+        </div>
+
+        <div>
+        
+        {/*Dette er links, som er oppe i const links.  */}
+        <Group gap={5} visibleFrom="sm" >
+          {items}
+
+        {/*Dette er ikonet som er ude i højre side af headeren.   */}
+          <IconUserCircle className={classes.icon}/>
+        </Group>
+        </div>
+        
         <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
       </Container>
     </header>
