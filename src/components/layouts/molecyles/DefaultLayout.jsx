cb7b283f57a511665eff655626ed7ba12d5eafaf
Monique-Artfull <134688072+Monique-Artfull@users.noreply.github.com>
2023-11-22 14:06:01 +0100

    loading

--- src/components/layouts/molecyles/DefaultLayout.jsx
+++ src/components/layouts/molecyles/DefaultLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from './Header'
+import {Loader} from './Loading'
 
 export default function DefaultLayout({children}) {
   return (
     <div>
       <Header/>
+      <Loader/>
       {children}
     </div>
   )
