16f973df4f243e1f52722d7bb30507c191fb0152
Julie <jc.lauesen@gmail.com>
2023-11-22 19:17:17 +0100

    loading siden

--- src/components/layouts/molecyles/Loading.jsx
+++ src/components/layouts/molecyles/Loading.jsx
@@ -1,22 +1,43 @@
+// import React from 'react';
+// import { useDisclosure } from '@mantine/hooks';
+// import { LoadingOverlay, Button, Group, Box } from '@mantine/core';
+
+// export function Loader() {
+//   const [visible, { toggle }] = useDisclosure(false);
+
+//   // Note that position: relative is required
+//   return (
+//     <>
+//       <Box pos="relative">
+//         <LoadingOverlay visible={visible} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
+//         {/* ...other content */}
+//       </Box>
+
+//       <Group position="center">
+//         <Button onClick={toggle}>Toggle overlay</Button>
+//       </Group>
+//     </>
+//   );
+// };
+// export default Loader;
+
+
+// Loading.js
 import React from 'react';
 import { useDisclosure } from '@mantine/hooks';
-import { LoadingOverlay, Button, Group, Box } from '@mantine/core';
+import { LoadingOverlay, Center, Container } from '@mantine/core';
 
-export function Loader() {
+const Loading = () => {
   const [visible, { toggle }] = useDisclosure(false);
 
-  // Note that position: relative is required
   return (
-    <>
-      <Box pos="relative">
+    <Container>
+      <Center style={{ minHeight: '5vh' }}>
         <LoadingOverlay visible={visible} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
-        {/* ...other content */}
-      </Box>
-
-      <Group position="center">
-        <Button onClick={toggle}>Toggle overlay</Button>
-      </Group>
-    </>
+        <button onClick={toggle}>Toggle Loading</button>
+      </Center>
+    </Container>
   );
 };
-export default Loader;
+
+export default Loading;
