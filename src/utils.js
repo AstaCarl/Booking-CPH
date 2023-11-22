6698f4ba2b54f2824ac5ef54541c6530acb15ac5
steffenkloster <steffenakloster@gmail.com>
2023-11-22 12:11:42 +0100

    getUser function

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export const getUser = () => {
+  if (!localStorage["sb-ofbgpdhnblfmpijyknvf-auth-token"]) {
+    return {
+      isLoggedIn: false,
+    };
+  }
+
+  const user = JSON.parse(
+    localStorage["sb-ofbgpdhnblfmpijyknvf-auth-token"]
+  )?.user;
+
+  return {
+    isLoggedIn: true,
+    userId: user.id,
+    firstName: user.user_metadata.first_name ?? "",
+    lastName: user.user_metadata.last_name ?? "",
+  };
+};
