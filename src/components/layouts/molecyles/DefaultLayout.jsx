91c67241b2588860f982936d70bfb4a1222a7181
Julie <jc.lauesen@gmail.com>
2023-12-12 19:12:28 +0100

    kommentar

--- src/components/layouts/molecyles/DefaultLayout.jsx
+++ src/components/layouts/molecyles/DefaultLayout.jsx
@@ -5,13 +5,17 @@ export default function DefaultLayout({ children }) {
   return (
     <div>
       {/*Header component */}
+      {/* Placering af Header komponenten øverst i layoutet */}
       <Header />
 
       {/*Main content */}
       <main>
+        {/* Wrapper-div for dynmaisk indhold (children) */}
         <div>{children}</div>
       </main>
-      <Footer/>
+
+      {/* Placering af Footer komponenten nederst i layoutet */}
+      <Footer />
     </div>
   );
 }
