c1fa9f04a9fc82c6ebde4159cc1a438bb1074050
AstaCarl <cph-ac300@cphbusiness.dk>
2023-12-13 10:39:46 +0100

    [user]

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -40,14 +40,14 @@ const Home = () => {
     }
 
     setUser(user);
-  }, [user]);
+  }, []);
 
   //Fetcher booking og rum data, når brugeren skifter.
   useEffect(() => {
     if (user !== null && Object.keys(user).length > 0 && user.isLoggedIn) {
       fetchData();
     }
-  }, []);
+  }, [user]);
 
   const fetchData = async () => {
     await fetchBooking();
